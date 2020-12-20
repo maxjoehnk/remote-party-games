cdfcf0b809647adf8479eb70d9ad973086bba06c
Max Jöhnk <maxjoehnk@gmail.com>
2020-12-20 18:41:27 +0100

    fix(taboo): progressbar doesn't reflect configured timer

--- applications/web-client/src/app/games/taboo/taboo-game.component.tsx
+++ applications/web-client/src/app/games/taboo/taboo-game.component.tsx
@@ -5,13 +5,16 @@ import { selectTabooScore, selectTabooTimeLeft } from '../../../store/selectors/
 import TabooGameArea from './taboo-game-area.component';
 import TabooPlayerList from './taboo-player-list.component';
 import GameHeader from '../../game-widgets/header/game-header';
+import { selectGameConfig } from '../../../store/selectors/lobby';
+import { TabooGameConfiguration } from '../../../contracts/taboo-game-configuration';
 
 const TabooGame = () => {
   const timeLeft = useSelector(selectTabooTimeLeft);
+  const config: TabooGameConfiguration = useSelector(selectGameConfig);
 
   return (
     <div className="game-taboo">
-      <GameHeader className="game-taboo__header" timer={{ maxTime: 60, timeLeft }}>
+      <GameHeader className="game-taboo__header" timer={{ maxTime: config.timer, timeLeft }}>
         <TabooGameScore />
       </GameHeader>
       <TabooPlayerList />
