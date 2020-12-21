2ccfc3acd1a52edbbf17c9930b73a28dd1fddc51
Max Jöhnk <maxjoehnk@gmail.com>
2020-12-21 16:43:04 +0100

    chore(web-client): update dependencies, replace babel-polyfill with core-js and regenerator-runtime

--- applications/web-client/src/main.tsx
+++ applications/web-client/src/main.tsx
@@ -1,4 +1,5 @@
-import 'babel-polyfill';
+import 'core-js/stable';
+import 'regenerator-runtime/runtime'
 import React from 'react';
 import * as ReactDom from 'react-dom';
 import Modal from 'react-modal';
