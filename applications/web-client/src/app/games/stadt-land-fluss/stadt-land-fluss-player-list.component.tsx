22f40c83c6056767c536377e523e31caa0bd9721
Max Jöhnk <maxjoehnk@gmail.com>
2021-05-10 07:37:13 +0200

    feat(stadt-land-fluss): sort players by rank
    fixes #24

--- applications/web-client/src/app/games/stadt-land-fluss/stadt-land-fluss-player-list.component.tsx
+++ applications/web-client/src/app/games/stadt-land-fluss/stadt-land-fluss-player-list.component.tsx
@@ -2,12 +2,16 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 import { selectStadtLandFlussPlayerScores } from '../../../store/selectors/stadt-land-fluss';
 import PlayerList, { PlayerItem } from '../../matchmaking/player-list/player-list.component';
+import { selectPlayerList } from '../../../store/selectors/lobby';
 
 const StadtLandFlussPlayerList = () => {
+  const players = useSelector(selectPlayerList);
   const scores = useSelector(selectStadtLandFlussPlayerScores);
 
+  const sortedPlayers = [...players].sort((lhs, rhs) => scores[rhs.id] - scores[lhs.id]);
+
   return (
-    <PlayerList canChangeTeam={false} className="game-stadt-land-fluss__player-list">
+    <PlayerList canChangeTeam={false} className="game-stadt-land-fluss__player-list" players={sortedPlayers}>
       {(text, player) => {
         const score = scores[player.id];
 
